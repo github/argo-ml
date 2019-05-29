f2ef911044a68ee8e2d1399bb981766adfac0334
inc0 <inc0@analytics-exploration-ead20c6.private-us-east-1.github.net>
2019-05-29 14:02:49 -0700

    added workflow api to hparam

--- hyperparam-controller/controller.py
+++ hyperparam-controller/controller.py
@@ -4,22 +4,6 @@ import json, yaml
 from kubernetes import client, config, watch
 
 
-group = "argoproj.io"
-version = "v1alpha1"
-plural = "hyperparamworkflows"
-
-
-config.load_incluster_config()
-
-
-namespace = 'default'
-api_client = client.ApiClient()
-custom_api = client.CustomObjectsApi(api_client)
-
-
-watch = watch.Watch(return_type=object)
-
-
 def generate_param_combinations(params):
     keys, values = zip(*params.items())
     experiments = [dict(zip(keys, v)) for v in itertools.product(*values)]
@@ -68,20 +52,39 @@ def generate_workflow(wf, experiments):
     pprint(wf['metadata'])
     return wf
 
+def main():
+    group = "argoproj.io"
+    version = "v1alpha1"
+    plural = "hyperparamworkflows"
 
-print("Starting loop")
-for event in watch.stream(custom_api.list_namespaced_custom_object, group, version, namespace, plural):
-    if event['type'] == 'ADDED':
-        hparams = event['raw_object']['spec']['hyperparams']
-        if event['raw_object']['spec']['algorithm'] == 'grid':
-            experiments = grid_search(hparams)
-            wf = generate_workflow(event['raw_object'], experiments)
-            try:
-                resp = custom_api.create_namespaced_custom_object(group, version, namespace, "workflows", wf, pretty=True)
-            except client.rest.ApiException:
-                continue
-            print(yaml.dump(wf))
-    if event['type'] == 'DELETED':
-        # TODO: This would be better managed with resource owners
-        name = event['raw_object']['metadata']['name']
-        custom_api.delete_namespaced_custom_object(group, version, namespace, "workflows", name=name, body=client.V1DeleteOptions())
+
+    config.load_incluster_config()
+
+
+    namespace = 'default'
+    api_client = client.ApiClient()
+    custom_api = client.CustomObjectsApi(api_client)
+
+
+    watch = watch.Watch(return_type=object)
+
+    print("Starting loop")
+    for event in watch.stream(custom_api.list_namespaced_custom_object, group, version, namespace, plural):
+        if event['type'] == 'ADDED':
+            hparams = event['raw_object']['spec']['hyperparams']
+            if event['raw_object']['spec']['algorithm'] == 'grid':
+                experiments = grid_search(hparams)
+                wf = generate_workflow(event['raw_object'], experiments)
+                try:
+                    resp = custom_api.create_namespaced_custom_object(group, version, namespace, "workflows", wf, pretty=True)
+                except client.rest.ApiException:
+                    continue
+                print(yaml.dump(wf))
+        if event['type'] == 'DELETED':
+            # TODO: This would be better managed with resource owners
+            name = event['raw_object']['metadata']['name']
+            custom_api.delete_namespaced_custom_object(group, version, namespace, "workflows", name=name, body=client.V1DeleteOptions())
+
+
+if __name__ == "__main__":
+    main()
