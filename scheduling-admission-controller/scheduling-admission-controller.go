1757f90a8db1a35d5664fe3114d31d24fb912d31
awmatheson <awmatheson@github.com>
2019-05-29 14:47:25 -0700

    basic https

---
+++ scheduling-admission-controller/scheduling-admission-controller.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	caCert, err := ioutil.ReadFile("client.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+	cfg := &tls.Config{
+		ClientAuth: tls.RequireAndVerifyClientCert,
+		ClientCAs:  caCertPool,
+	}
+	srv := &http.Server{
+		Addr:      ":8443",
+		Handler:   &handler{},
+		TLSConfig: cfg,
+	}
+	log.Print("Starting the service...")
+	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
+}
+
+type handler struct{}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("PONG\n"))
+}
