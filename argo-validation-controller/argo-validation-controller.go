5e71d5f8c266d1883b87ea3046284d3f1969c529
inc0 <inc0@analytics-exploration-ead20c6.private-us-east-1.github.net>
2019-05-30 13:39:42 -0700

    rename and compile

---
+++ argo-validation-controller/argo-validation-controller.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
+	"encoding/json"
+	"log"
+	"fmt"
+	"bytes"
+	"net/http"
+	"k8s.io/api/admission/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	// validation "github.com/argoproj/argo/workflow/validate"
+	wfv1 "github.com/argoproj/argo/pkg/apis/workflow/v1alpha1"
+)
+
+func main() {
+	caCert, err := ioutil.ReadFile("client.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+	cfg := &tls.Config{
+		ClientAuth: tls.RequireAndVerifyClientCert,
+		ClientCAs:  caCertPool,
+	}
+	srv := &http.Server{
+		Addr:      ":8443",
+		Handler:   &handler{},
+		TLSConfig: cfg,
+	}
+	log.Print("Starting the service...")
+	log.Fatal(srv.ListenAndServeTLS("cert.pem", "key.pem"))
+}
+
+type handler struct{}
+
+func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	
+	b, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	var allowed bool
+	var validationError string
+
+	wf, err := getResource(b)
+	if err != nil {
+		allowed = false
+		validationError = fmt.Sprintf("Error while generating workflow %s", err)
+	}
+	fmt.Sprintf("workflow %s", wf)
+
+	// get a json string here
+
+	// pass a json string here
+	if err != nil {
+		validationError = fmt.Sprintf("Validation error %s", err)
+		allowed = false
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	reviewStatus := v1beta1.AdmissionResponse{}
+
+	if allowed {
+		reviewStatus.Allowed = true
+	} else {
+		reviewStatus.Allowed = false
+		reviewStatus.Result = &metav1.Status{
+			Message: validationError,
+		}
+
+	}
+	output, _ := json.Marshal(reviewStatus)
+  	w.Write(output)
+}
+
+// function to ping the hparam api
+func getResource(jsonData []byte) ([]byte, error) {
+	jsonValue, _ := json.Marshal(jsonData)
+	response, err := http.Post("analytics-exploration-ead20c6.private-us-east-1.github.net:5000/workflow", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		log.Printf("The HTTP request failed with error %s\n", err)
+		r := []byte("")
+		return r, err
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		return data, nil
+	}
+}
+
+
+// function to validate the results using argoproj validation code
+func validate(jsonStr string) *wfv1.Workflow {
+	wf := unmarshalWf(jsonStr)
+	return wf
+}
+
+func unmarshalWf(jsonStr string) *wfv1.Workflow {
+	var wf wfv1.Workflow
+	err := json.Unmarshal([]byte(jsonStr), &wf)
+	if err != nil {
+		panic(err)
+	}
+	return &wf
+}
+
